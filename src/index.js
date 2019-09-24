b997aecdf5e9cd1fb13f1209f7b27543262cc05d
Conrad Wright <easp.conrad@gmail.com>
2019-09-24 06:11:20 -0600

    Add ApolloProvider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import RootComponent from './App'
+import * as serviceWorker from './serviceWorker'
+import { ApolloProvider } from '@apollo/react-hooks'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient, { gql } from 'apollo-boost'
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+})
+
+client
+  .query({
+    query: gql`
+      {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `,
+  })
+  .then(result => console.log(result))
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <RootComponent />
+  </ApolloProvider>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
